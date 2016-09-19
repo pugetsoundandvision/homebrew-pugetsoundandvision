6b04174fd843c8b4f682d47dd5c9619caa36ab05
Andrew Weaver <weevz@uw.edu>
2016-09-19 14:33:57 -0400

    Update audiotools.rb

--- audiotools.rb
+++ audiotools.rb
@@ -2,8 +2,8 @@ require 'formula'
 class Audiotools < Formula
   desc ""
   homepage ""
-  url "https://github.com/pugetsoundandvision/audiotools/archive/v0.0.02.zip"
-  sha256 "09011d9bb928e1021a65c51095f9436bb69dea2a626687144e1ec2d47978b739"
+  url "https://github.com/pugetsoundandvision/audiotools/archive/v0.0.03.zip"
+  sha256 "82ae5fe5f23b4acbaeb737ddd9a6a447dee735709d396ed84fd8c1753dd2470c"
   
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-libass', 'with-freetype', 'with-faac' ]
   depends_on 'cask'
