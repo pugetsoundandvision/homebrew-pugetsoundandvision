5faa159efb2ba009d79630ecb117bcef288b03e9
Andrew Weaver <weevz@uw.edu>
2017-03-16 15:43:50 -0400

    Update audiotools.rb

--- audiotools.rb
+++ audiotools.rb
@@ -6,7 +6,7 @@ class Audiotools < Formula
   sha256 "db61c578f900f8170464571a5f52dbf657e5c8c80fcb3153ae9cb3d4ff46b0a5"
   head "https://github.com/pugetsoundandvision/audiotools.git"
   revision 1
-  
+
   depends_on "sdl"
   depends_on "ffmpeg" => [ 'with-sdl2', 'with-freetype', 'with-opus']
   depends_on 'cask'
@@ -35,6 +35,11 @@ class Audiotools < Formula
   end
   
   def post_install;
+    unless `brew ls --versions audiorecorder 2>&1 | head -n1`.include? 'audiorecorder'
+      puts "Installing audiorecorder"
+      system `echo 'brew tap amiaopensource/amiaos'`
+      system `echo 'brew install audiorecorder --ignore-dependencies'`
+    end
       puts "Checking for dependency Textmate. If Textmate is not found will attempt to install"
       if File.exist? File.expand_path '/Applications/TextMate.app'
         puts 'TextMate was found'
