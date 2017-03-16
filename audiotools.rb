d5ef38725305a8435a1a4a0f0499a1f5f47dfe66
Andrew Weaver <weevz@uw.edu>
2017-03-16 15:42:27 -0400

    Update audiotools.rb

--- audiotools.rb
+++ audiotools.rb
@@ -5,6 +5,7 @@ class Audiotools < Formula
   url "https://github.com/pugetsoundandvision/audiotools/archive/v0.2.01.zip"
   sha256 "db61c578f900f8170464571a5f52dbf657e5c8c80fcb3153ae9cb3d4ff46b0a5"
   head "https://github.com/pugetsoundandvision/audiotools.git"
+  revision 1
   
   depends_on "sdl"
   depends_on "ffmpeg" => [ 'with-sdl2', 'with-freetype', 'with-opus']
@@ -15,6 +16,9 @@ class Audiotools < Formula
   depends_on 'md5deep'
   depends_on 'imagesnap'
   depends_on 'mediaconch'
+  depends_on 'mpv'
+  depends_on 'sox'
+  depends_on 'dialog'
 
 
  
