c8ff73569aedcec145b3ebccf9f14d8dcde8a612
Andrew Weaver <weevz@uw.edu>
2017-03-17 10:55:05 -0400

    Update audiotools.rb

--- audiotools.rb
+++ audiotools.rb
@@ -5,10 +5,10 @@ class Audiotools < Formula
   url "https://github.com/pugetsoundandvision/audiotools/archive/v0.2.01.zip"
   sha256 "db61c578f900f8170464571a5f52dbf657e5c8c80fcb3153ae9cb3d4ff46b0a5"
   head "https://github.com/pugetsoundandvision/audiotools.git"
-  revision 2
+  revision 3
 
   depends_on "sdl"
-  depends_on "ffmpeg" => ["with-sdl2", "with-freetype", "with-opus"]
+  depends_on "ffmpeg" => ["with-sdl2", "with-freetype", "with-opus", "--with-fdk-aac"]
   depends_on "cask"
   depends_on "mediainfo"
   depends_on "bagit"
