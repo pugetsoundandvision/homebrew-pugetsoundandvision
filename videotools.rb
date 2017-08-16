7375b443cd820de211363263fe1334388039b21b
Andrew Weaver <weevz@uw.edu>
2017-08-16 15:04:45 -0700

    Update videotools.rb

--- videotools.rb
+++ videotools.rb
@@ -1,43 +1,24 @@
 require "formula"
-class Audiotools < Formula
+class Videotools < Formula
   desc ""
   homepage ""
-  url "https://github.com/pugetsoundandvision/audiotools/archive/v0.2.01.zip"
-  sha256 "db61c578f900f8170464571a5f52dbf657e5c8c80fcb3153ae9cb3d4ff46b0a5"
-  head "https://github.com/pugetsoundandvision/audiotools.git"
-  revision 4
+  url "https://github.com/pugetsoundandvision/videotools/archive/2017-09-15.tar.gz"
+  sha256 "f0f80a83f3d54ce95897be06db6bf99d4c9c0ec5c0d0a57dd845e695dec8141e"
+  head "https://github.com/pugetsoundandvision/videotools.git"
 
   depends_on "sdl"
   depends_on "ffmpeg" => ["with-sdl2", "with-freetype", "with-opus", "with-fdk-aac"]
   depends_on "cask"
   depends_on "mediainfo"
   depends_on "bagit"
-  depends_on "flac"
   depends_on "md5deep"
-  depends_on "imagesnap"
-  depends_on "mediaconch"
   depends_on "mpv"
-  depends_on "sox"
-  depends_on "dialog"
-
+  
   def install
-    bin.install "uwmetaedit"
-    bin.install "audioaip"
-    bin.install "verifychecksums"
-    etc.install "audioaip_mediaconch_policy.xml"
-    if File.exist? File.expand_path "/usr/local/bin/bwfmetaedit"
-      puts "BWF Metaedit confirmed"
-    else
-      bin.install "bwfmetaedit"
-    end
+    bin.install "videoaip"
   end
 
   def post_install
-    unless `brew ls --versions audiorecorder 2>&1 | head -n1`.include? "audiorecorder"
-      puts "Installing audiorecorder"
-      system `echo 'brew tap amiaopensource/amiaos'`
-      system `echo 'brew install audiorecorder --ignore-dependencies'`
-    end
     puts "Checking for dependency Textmate. If Textmate is not found will attempt to install"
     if File.exist? File.expand_path "/Applications/TextMate.app"
       puts "TextMate was found"
