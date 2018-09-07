2862e99f4b78d58cc745ba03ca718062f2613df3
Andrew Weaver <weevz@uw.edu>
2018-09-07 15:36:19 -0700

    2018-09-07 release

--- ltotools.rb
+++ ltotools.rb
@@ -2,9 +2,9 @@ require "formula"
 class Ltotools < Formula
   desc ""
   homepage ""
-  url "https://github.com/pugetsoundandvision/lto_tools/archive/2018-09-04.tar.gz"
-  version "2018-09-04"
-  sha256 "9fb986fc8dadf357351ba32427c5dcf56154ba89bfea50cfc6f656dee75a148d"
+  url "https://github.com/pugetsoundandvision/lto_tools/archive/2018-09-07.tar.gz"
+  version "2018-09-07"
+  sha256 "10b786cfabb86033e9014e306e5a984e5caa5a268d35f20f11a0ed40e493e2bf"
   head "https://github.com/pugetsoundandvision/lto_tools.git"
   
   
