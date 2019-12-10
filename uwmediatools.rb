578069459f2ed317f38d8d6e413ef259f851dfdb
Andrew Weaver <theandrewjw@gmail.com>
2019-12-10 15:27:03 -0800

    Update uwmediatools.rb

--- uwmediatools.rb
+++ uwmediatools.rb
@@ -1,4 +1,3 @@
-Learn more or give us feedback
 require "formula"
 class Videotools < Formula
   desc ""
@@ -7,6 +6,7 @@ class Videotools < Formula
   version "2019-12-11"
   sha256 "13520a842440a44831013735f94df6f98b4094b0557b9761bb781e9f5eca04ac"
   head "https://github.com/pugetsoundandvision/uwmediascripts.git"
+  revision 1
 
   depends_on "ffmpeg"
   depends_on "mediaconch"
